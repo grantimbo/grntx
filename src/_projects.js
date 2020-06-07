2d67aa51f8636ad0d44026d3786a3ecb8a044b25
grantimbo <grant.imbo@gmail.com>
2020-06-07 23:07:56 +0800

    organize projects

--- src/_projects.js
+++ src/_projects.js
@@ -1,58 +1,242 @@
 export const projects = [
     {
-        title: 'Neon Life',
-        slug: 'neon-life',
+        title: 'GEARS',
+        slug: 'gears',
         date: 'July 2015',
-        thumbnail: 'https://grantimbo.com/wp-content/uploads/neon-life-grantimbo-thumb.jpg',
-        content: `hello this is the content `
+        tags: ['featured', 'motion'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/grantimbo-gears-thumb.jpg',
+        content: `
+        <h1>Motion Design / Title Opener</h1>
+        <p>Tools : Cinema 4D, Octane Render, Sony Vegas and After Effects.</p>
+        <p>Modeling, texturing, lighting, and animation in Cinema 4D.</p>
+        <p>Rendered using Octane Render.</br>
+        Sound Design done in Sony Vegas.</br>
+        Compositing in After Effects.</br>
+        </p>
+        <iframe title="GEARS - Title Opener" src="https://player.vimeo.com/video/345348719" width="500" height="281" frameborder="0" allow="autoplay; fullscreen" allowfullscreen=""></iframe>
+        <h3>Animatic</h3>
+        <iframe title="GEARS - Animatic" src="https://player.vimeo.com/video/345348200" width="426" height="240" frameborder="0" allow="autoplay; fullscreen" allowfullscreen=""></iframe>
+        <p>Modelling, texturing, lighting and animation done in Cinema 4D</p>
+        <h3>Sound Design</h3>
+        <iframe title="GEARS - Sound Design" src="https://player.vimeo.com/video/345348115" width="500" height="281" frameborder="0" allow="autoplay; fullscreen" allowfullscreen=""></iframe>
+        <p>Sound Design done in Sony Vegas Pro</p>
+        <h3>Stills</h3>
+        <img src="https://grantimbo.com/wp-content/uploads/gears-01.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/gears-06.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/gears-05.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/gears-02.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/gears-03.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/gears-04.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/gears-07.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/gears-09.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/gears-08.jpg">
+        `
     },
     {
-        title: 'Keep Calm',
-        slug: 'keep-calm',
-        date: 'July 2014',
-        thumbnail: 'https://grantimbo.com/wp-content/uploads/keep-calm-grantimbo-thumb.png',
-        content: `keep calm`
-    },
-    {
-        title: 'Chada Valencia',
-        slug: 'chada-valencia',
-        date: 'July 2016',
-        thumbnail: 'https://grantimbo.com/wp-content/uploads/testtt0003-300x188.jpg',
-        content: `chada valencia`
-    },
-    {
-        title: 'Widebody Evo',
-        slug: 'wide-evo',
+        title: 'Wide Body Evolution',
+        slug: 'wide-body-evo',
         date: 'July 2019',
+        tags: ['featured', 'modelling'],
         thumbnail: 'https://grantimbo.com/wp-content/uploads/1-6.jpg',
-        content: `wide evo`
+        content: `
+        <h1>Motion Design / Title Opener</h1>
+        <iframe title="3D Modelling Timelapse - Custom WIdebody Lancer Evo IV" width="500" height="281" src="https://www.youtube.com/embed/S7XCEfg7tnY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+        <p>Tools : Cinema 4D, Octane Render, Photoshop</p>
+        <p>Cyberpunk Style</p>
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-cyberpunk-evo-720x720.jpg">
+        <p>Green Hornet Evo</p>
+        <img src="https://grantimbo.com/wp-content/uploads/green_evo-720x720.png">
+        <p>Shiny Evo</p>
+        <img src="https://grantimbo.com/wp-content/uploads/shiny_evo_02-720x720.jpg">
+        <p>Chinatown Evo</p>
+        <img src="https://grantimbo.com/wp-content/uploads/evo_night-720x720.png">
+        <p>The Blue Evo</p>
+        <img src="https://grantimbo.com/wp-content/uploads/blue_evo-720x720.png">
+        `
     },
     {
-        title: 'Abstract Renders',
-        slug: 'abstract-renders',
-        date: 'July 2015',
-        thumbnail: 'https://grantimbo.com/wp-content/uploads/grantimbo-abstract-renders-thumb.jpg',
-        content: `hello this is the content `
+        title: 'Photo Manipulations',
+        slug: 'photo-manipulations',
+        date: 'July 2019',
+        tags: ['featured', 'photomanipulation'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/photomanip-thumb.jpg',
+        content: `
+        <p>Cyberpunk Style</p>
+        <img src="https://grantimbo.com/wp-content/uploads/2-4-768x960.jpg">
+        <p>Green Hornet Evo</p>
+        <img src="https://grantimbo.com/wp-content/uploads/sunrise-768x960.jpg">
+        <p>Shiny Evo</p>
+        <img src="https://grantimbo.com/wp-content/uploads/Obscure-768x960.jpg">
+        <h1>Breakdowns</h1>
+        <iframe width="626" height="330" src="https://www.youtube.com/embed/rVDLTEexbfo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="626" height="330" src="https://www.youtube.com/embed/9AwXxvUaCaA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="626" height="330" src="https://www.youtube.com/embed/pVmUFEQlkwg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        `
     },
+    
     {
         title: 'Motion Design Compilation',
         slug: 'motion-compilation',
         date: 'July 2014',
+        tags: ['typography'],
         thumbnail: 'https://grantimbo.com/wp-content/uploads/grantimbo-motion-thumb.jpg',
+        content: `keep calm`
+    },
+    {
+        title: 'Slanttex',
+        slug: 'slanttex',
+        date: 'July 2014',
+        tags: ['web dev'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/grantimbo-thumb-slanttex.jpg',
         content: `keep calm`
     },
     {
         title: 'Dumzville',
         slug: 'dumzville',
-        date: 'July 2016',
+        date: 'July 2015',
+        tags: ['typography'],
         thumbnail: 'https://grantimbo.com/wp-content/uploads/4-1-300x188.jpg',
+        content: `
+        <img src="https://grantimbo.com/wp-content/uploads/2-3.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/3-3.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/4-1.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/0-1.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/1-5.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/5-1.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/6.jpg">
+        `
+    },
+    {
+        title: 'Type Compilation',
+        slug: 'type-compilation',
+        date: 'July 2016',
+        tags: ['typography'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/type-compilation-thumb.jpg',
         content: `chada valencia`
     },
     {
         title: 'Product Visualizations',
         slug: 'product-visualizations',
         date: 'July 2019',
+        tags: ['typography'],
         thumbnail: 'https://grantimbo.com/wp-content/uploads/photo_1240433985969247-300x188.jpg',
         content: `wide evo`
+    },
+    {
+        title: 'LiquiPal Product Visualization',
+        slug: 'liquipal',
+        date: 'July 2014',
+        tags: ['product render'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/liquipal-grantimbo-thumb.jpg',
+        content: `
+        <img src="https://grantimbo.com/wp-content/uploads/keep_calm_header.png">
+        <img src="https://grantimbo.com/wp-content/uploads/keep_calm_preview_01.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/keep_calm_preview.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/keep_calm_preview_02.jpg">
+        `
+    },
+    {
+        title: 'Chada Valencia',
+        slug: 'chada-valencia',
+        date: 'July 2014',
+        tags: ['typography'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/testtt0003-300x188.jpg',
+        content: `
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-01-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-02-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-03-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-04-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-05-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-06-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-07-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-08-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-09-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-10-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-11-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-12-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-13-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-14-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-15-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/grantimbo-chada-valencia-16-1080x1080.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/testtt0003-1526x1080.jpg">
+        `
+    },
+    {
+        title: 'Abstract Renders',
+        slug: 'abstract-renders',
+        date: 'July 2015',
+        tags: ['typography'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/grantimbo-abstract-renders-thumb.jpg',
+        content: `<img src="https://grantimbo.com/wp-content/uploads/metabol-864x1080.jpg"> `
+    },
+    {
+        title: 'Life Goes On',
+        slug: 'life-goes-on',
+        date: 'July 2015',
+        tags: ['typography'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/lifegoeson-grantimbo-thumb.png',
+        content: `<img src="https://grantimbo.com/wp-content/uploads/behance-bg.jpg">`
+    },
+    {
+        title: 'KTM Model',
+        slug: 'ktm-model',
+        date: 'August 2015',
+        tags: ['3d-modelling'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/07-300x188.jpg',
+        content: `<img src="https://grantimbo.com/wp-content/uploads/behance-bg.jpg">`
+    },
+    {
+        title: 'Do it Now',
+        slug: 'do-it-now',
+        date: 'July 2015',
+        tags: ['typography'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/do-it-now-grantimbo-thumb.jpg',
+        content: `<img src="https://grantimbo.com/wp-content/uploads/behance-bg.jpg">`
+    },
+    {
+        title: 'Type G',
+        slug: 'type-g',
+        date: 'September 2014',
+        tags: ['typography'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/type-g-grantimbo-thumb.png',
+        content: `<img src="https://grantimbo.com/wp-content/uploads/behance-bg.jpg">`
+    },
+    {
+        title: 'Summer Time',
+        slug: 'summer-time',
+        date: 'April 2014',
+        tags: ['typography'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/summer-time-grantimbo-thumb.png',
+        content: `<img src="https://grantimbo.com/wp-content/uploads/behance-bg.jpg">`
+    },
+    {
+        title: 'Neon Life',
+        slug: 'neon-life',
+        date: 'April 2014',
+        tags: ['typography'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/neon-life-grantimbo-thumb.jpg',
+        content: `<img src="https://grantimbo.com/wp-content/uploads/neon-life-grantimbo.png"> `
+    },
+    {
+        title: 'Keep Calm',
+        slug: 'keep-calm',
+        date: 'October 2013',
+        tags: ['typography'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/keep-calm-grantimbo-thumb.png',
+        content: `
+        <img src="https://grantimbo.com/wp-content/uploads/keep_calm_header.png">
+        <img src="https://grantimbo.com/wp-content/uploads/keep_calm_preview_01.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/keep_calm_preview.jpg">
+        <img src="https://grantimbo.com/wp-content/uploads/keep_calm_preview_02.jpg">
+        `
+    },
+    {
+        title: 'Discipline',
+        slug: 'discipline',
+        date: 'July 2013',
+        tags: ['typography'],
+        thumbnail: 'https://grantimbo.com/wp-content/uploads/type-g-grantimbo-thumb.png',
+        content: `<img src="https://grantimbo.com/wp-content/uploads/behance-bg.jpg">`
     }
+
 ]
