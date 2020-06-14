74697eb7ad4390aaefb9b0b5a48bc1b2957c30fc
grantimbo <grant.imbo@gmail.com>
2020-06-14 19:32:56 +0800

    remove api host

--- src/App.svelte
+++ src/App.svelte
@@ -7,16 +7,15 @@
 	import About from './pages/About.svelte';
 	import NotFound from './pages/NotFound.svelte';
 	
-	let apiHost = `https://grantimbo.com/`;
 
 </script>
 
 <main>
 	<Router>
-		<Route path="/"><Home /></Route>
-		<Route path="projects"><Projects {apiHost}/></Route>
+		<Route path="/"><Home/></Route>
+		<Route path="projects"><Projects/></Route>
 		<Route path="project-type/:cat" let:params><Category cat="{params.cat}"/></Route>
-		<Route path="projects/:slug" let:params><Projects {apiHost} slug="{params.slug}"/></Route>
+		<Route path="projects/:slug" let:params><Projects slug="{params.slug}"/></Route>
 		<Route path="services"><Services/></Route>
 		<Route path="about"><About/></Route>
 		<Route><NotFound/></Route>
